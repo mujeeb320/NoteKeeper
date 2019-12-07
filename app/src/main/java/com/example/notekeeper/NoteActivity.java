360b54815f3d0e0f13ab44f1c8613398d4ad8285
mujeeb <mujeebsiddiq98@gmail.com>
2019-12-07 12:50:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/notekeeper/NoteActivity.java
@@ -0,0 +1,70 @@
+package com.example.notekeeper;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.List;
+
+public class NoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        Spinner spinnerCourses=(Spinner) findViewById(R.id.spinner_courses);
+
+        List<CourseInfo> listCourses = DataManager.getInstance().getCourses();
+        ArrayAdapter<CourseInfo> adapterCourse =
+                new ArrayAdapter<CourseInfo>
+                        (this,android.R.layout.simple_spinner_dropdown_item,listCourses);
+
+        adapterCourse.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerCourses.setAdapter(adapterCourse);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
